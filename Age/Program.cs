02c4d28462d7043779776b5332d97d34c0a2d054
nj <niklas@julit.dk>
2022-03-21 13:51:31 +0100

    Edited some small things

--- Age/Program.cs
+++ Age/Program.cs
@@ -9,25 +9,22 @@ namespace Age
             Console.WriteLine("What is your name?");
             string name = Console.ReadLine();
             Console.WriteLine("What is your age");
-            int age = int.Parse(Console.ReadLine());    
+            int age = int.Parse(Console.ReadLine());
             if (age <= 3)
             {
                 Console.WriteLine(name + ", du må gå med ble");
             }
-            else if (age <= 15)
+            else if (age > 3 && age <= 15)
             {
                 Console.WriteLine(name + ", du må ingenting");
             }
-            else if (age < 18)
+            else if (age > 15 && age < 18)
             {
-                Console.WriteLine(name+ ", du må drikke");
+                Console.WriteLine(name + ", du må drikke");
             }
-            else if (age <= 18)
+            else
             {
                 Console.WriteLine(name + ", du må stemme og køre bil");
-            }
-            else {
-                Console.WriteLine("Put in a valid number");
             }
         }
     }
